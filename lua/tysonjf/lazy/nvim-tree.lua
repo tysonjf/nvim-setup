db0f437a2840f47104ee006eaa08a161d4a06723
Tyson <100465135+DieMantra@users.noreply.github.com>
2024-05-22 21:08:50 +1000

    added some extra vim stuff :\

--- lua/tysonjf/lazy/nvim-tree.lua
+++ lua/tysonjf/lazy/nvim-tree.lua
@@ -7,7 +7,10 @@ return {
 	},
 	config = function()
 		require("nvim-tree").setup({
-	})
+			
+		})
+		-- Key mapping to toggle nvim-tree
+		vim.api.nvim_set_keymap('n', '<leader>e', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
 	end,
 }
 
